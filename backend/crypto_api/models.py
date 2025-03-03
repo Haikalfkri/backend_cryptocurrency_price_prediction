6ad3e9374865abafae1a12e901e9fccb9a8d9349
Haikal Fikri <haikaljr1245@gmail.com>
2025-03-03 12:59:42 +0700

    Make CryptoData model with partition

--- backend/crypto_api/models.py
+++ backend/crypto_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class CryptoData(models.Model):
+    coint_name = models.CharField(max_length=255)
+    date = models.DateField()
+    open_price = models.DecimalField(max_digits=10, decimal_places=2)
+    high_price = models.DecimalField(max_digits=10, decimal_places=2)
+    low_price = models.DecimalField(max_digits=10, decimal_places=2)
+    close = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.DecimalField(max_digits=20, decimal_places=2)
+
+    class Meta:
+        db_table = 'crypto_data'
+        managed = False
+
+    def __str__(self):
+        return self.coint_name
